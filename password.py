81b21bd77e28ca64bd438dfc14cc831cc5d69ff9
YiTing8796 <73864807+YiTing8796@users.noreply.github.com>
2020-11-05 23:12:39 +0800

    Update password.py

--- password.py
+++ password.py
@@ -1,11 +1,14 @@
+password = 'a123456'
 i = 3
 while i > 0 :
+	i = i - 1
 	pw = input('請輸入密碼：')
-	if pw == 'a123456' :
+	if pw == password :
 		print('登入成功')
 		break
 	else :
-		i = i - 1
-		print('密碼錯誤！還有', i, '次機會')
+		print('密碼錯誤')
+		if i > 0 :
+			print('密碼錯誤！還有', i, '次機會')
 
 
