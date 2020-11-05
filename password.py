813cfda3ebde85edc52ce068b53ade3e6d219e51
Hsieh Yi Ting <hsiehyiting@Hsiehde-MacBook-Pro.local>
2020-11-05 23:08:59 +0800

    Updated

---
+++ password.py
@@ -0,0 +1,14 @@
+password = 'a123456'
+i = 3
+while i > 0 :
+	i = i - 1
+	pw = input('請輸入密碼：')
+	if pw == password :
+		print('登入成功')
+		break
+	else :
+		print('密碼錯誤')
+		if i > 0 :
+			print('密碼錯誤！還有', i, '次機會')
+
+
